8fb64f036551df38c3099d332c9e4c3c2b01bea1
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-09-07 12:36:05 +0300

    Refactor filter numbers directive

--- src/app/utilities/filterNumbers.directive.ts
+++ src/app/utilities/filterNumbers.directive.ts
@@ -7,30 +7,11 @@ import { NgControl } from '@angular/forms';
 export class FilterNumberDirective {
   constructor(private _el: ElementRef) {}
 
-  @HostListener('keydown', ['$event']) onInputChange(e: any) {
-    if (
-      [46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
-      // Allow: Ctrl+A
-      (e.keyCode === 65 && (e.ctrlKey || e.metaKey)) ||
-      // Allow: Ctrl+C
-      (e.keyCode === 67 && (e.ctrlKey || e.metaKey)) ||
-      // Allow: Ctrl+V
-      (e.keyCode === 86 && (e.ctrlKey || e.metaKey)) ||
-      // Allow: Ctrl+X
-      (e.keyCode === 88 && (e.ctrlKey || e.metaKey)) ||
-      // Allow: home, end, left, right
-      (e.keyCode >= 35 && e.keyCode <= 39)
-    ) {
-      // let it happen, don't do anything
-      return;
-    }
-    if (!/^[0-9]*\.?[0-9]*$/.test(e.key)) {
-      e.preventDefault();
-    }
-    if (e.target.value.includes('.')) {
-      if (!/[0-9]/.test(e.key)) {
-        e.preventDefault();
-      }
+  @HostListener('input', ['$event']) onInputChange(event: KeyboardEvent) {
+    const initalValue = this._el.nativeElement.value;
+    this._el.nativeElement.value = initalValue.replace(/[^0-9.]*/g, '');
+    if (initalValue !== this._el.nativeElement.value) {
+      event.stopPropagation();
     }
   }
 }
